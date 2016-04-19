65f921991e48f2a7bb296afd0fc1061ea8af273b
Steve Fosdick <steve@fosdick.me.uk>
2016-04-19 15:45:04 +0100

    Move opening the ttyUSB port back into the child process to avoid lockups

--- src/cc-termios.c
+++ src/cc-termios.c
@@ -18,7 +18,6 @@ struct _cc_ctx {
     logger_t       *logger;
     const char     *db_conn;
     const char     *port;
-    int            fd;
     struct termios tio;
     
 };
@@ -43,43 +42,51 @@ static void timer_cb(int sig)
 }
 
 static int child_proc(cc_ctx_t *ctx) {
+    int status, fd, init_port = 1;
     struct itimerval it;
-    int init_port = 1;
     unsigned char buf[BUF_SIZE];
     ssize_t nbytes;
 
-    it.it_interval.tv_sec = INTERVAL;
-    it.it_interval.tv_usec = 0;
-    it.it_value.tv_sec = INTERVAL;
-    it.it_value.tv_usec = 0;
-    if (setitimer(ITIMER_REAL, &it, NULL) == 0) {
-	log_msg("initialisation complete, begin main loop");
-	while (!exit_requested) {
-	    if (init_port) {
-		if (tcsetattr(ctx->fd, TCSANOW, &ctx->tio) == -1) {
-		    log_syserr("unable to set terminal attributes on port '%s'",
-			       ctx->port);
-		    return 16;
+    if ((fd = open(ctx->port, O_RDONLY | O_NONBLOCK)) >= 0) {
+	it.it_interval.tv_sec = INTERVAL;
+	it.it_interval.tv_usec = 0;
+	it.it_value.tv_sec = INTERVAL;
+	it.it_value.tv_usec = 0;
+	if (setitimer(ITIMER_REAL, &it, NULL) == 0) {
+	    log_msg("initialisation complete, begin main loop");
+	    status = 0;
+	    while (!exit_requested) {
+		if (init_port) {
+		    if (tcsetattr(fd, TCSANOW, &ctx->tio) == -1) {
+			log_syserr("unable to set terminal attributes "
+				   "on port '%s'", ctx->port);
+			status = 16;
+			break;
+		    }
+		    cb_count = init_port = 0;
 		}
-		cb_count = init_port = 0;
-	    }
-	    if ((nbytes = read(ctx->fd, buf, sizeof buf)) > 0) {
-		logger_data(ctx->logger, buf, nbytes);
-		if (nbytes > 1)
-		    cb_count = 0;
-	    } else {
-		if (cb_count > 1) {
-		    log_msg("port locked, re-initialising");
-		    init_port = 1;
+		if ((nbytes = read(fd, buf, sizeof buf)) > 0) {
+		    logger_data(ctx->logger, buf, nbytes);
+		    if (nbytes > 1)
+			cb_count = 0;
+		} else {
+		    if (cb_count > 1) {
+			log_msg("port locked, re-initialising");
+			init_port = 1;
+		    }
 		}
 	    }
+	    log_msg("shutting down on receipt of signal #%d", exit_requested);
+	} else {
+	    log_syserr("unable to set interval timer");
+	    status = 14;
 	}
-	log_msg("shutting down on receipt of signal #%d", exit_requested);
-	return 0;
+	close(fd);
     } else {
-	log_syserr("unable to set interval timer");
-	return 14;
+	log_syserr("unable to open port '%s'", ctx->port);
+	status = 10;
     }
+    return status;
 }
 
 int cc_termios(cc_ctx_t *ctx)
@@ -88,37 +95,33 @@ int cc_termios(cc_ctx_t *ctx)
     struct sigaction sa;
 
     if ((ctx->logger = logger_new(ctx->db_conn))) {
-	if ((ctx->fd = open(ctx->port, O_RDONLY | O_NONBLOCK)) >= 0) {
-	    memset(&sa, 0, sizeof sa);
-	    sa.sa_handler = exit_handler;
-	    if (sigaction(SIGTERM, &sa, NULL) == 0) {
-		if (sigaction(SIGINT, &sa, NULL) == 0) {
-		    sa.sa_handler = timer_cb;
-		    if (sigaction(SIGALRM, &sa, NULL) == 0) {
-			ctx->tio.c_iflag = IGNBRK | IGNCR;
-			ctx->tio.c_oflag = 0;
-			ctx->tio.c_cflag = CS8 | CREAD | CLOCAL;
-			ctx->tio.c_lflag = 0;
-			cfsetospeed(&ctx->tio, B57600);
-			cfsetispeed(&ctx->tio, B57600);
-			ctx->tio.c_cc[VMIN] = 1;
-			ctx->tio.c_cc[VTIME] = 0;
-			status = cc_daemon_fork(pid_file, child_proc, ctx);
-		    } else {
-			log_syserr("unable to handler for SIGALRM");
-			status = 13;
-		    }
+	log_msg("port open");
+	memset(&sa, 0, sizeof sa);
+	sa.sa_handler = exit_handler;
+	if (sigaction(SIGTERM, &sa, NULL) == 0) {
+	    if (sigaction(SIGINT, &sa, NULL) == 0) {
+		sa.sa_handler = timer_cb;
+		if (sigaction(SIGALRM, &sa, NULL) == 0) {
+		    ctx->tio.c_iflag = IGNBRK | IGNCR;
+		    ctx->tio.c_oflag = 0;
+		    ctx->tio.c_cflag = CS8 | CREAD | CLOCAL;
+		    ctx->tio.c_lflag = 0;
+		    cfsetospeed(&ctx->tio, B57600);
+		    cfsetispeed(&ctx->tio, B57600);
+		    ctx->tio.c_cc[VMIN] = 1;
+		    ctx->tio.c_cc[VTIME] = 0;
+		    status = cc_daemon_fork(pid_file, child_proc, ctx);
 		} else {
-		    log_syserr("unable to set handler for SIGINT");
-		    status = 12;
+		    log_syserr("unable to handler for SIGALRM");
+		    status = 13;
 		}
 	    } else {
-		log_syserr("unable to set handler for SIGTERM");
-		status = 11;
+		log_syserr("unable to set handler for SIGINT");
+		status = 12;
 	    }
 	} else {
-	    log_syserr("unable to open port '%s'", ctx->port);
-	    status = 10;
+	    log_syserr("unable to set handler for SIGTERM");
+	    status = 11;
 	}
 	logger_free(ctx->logger);
     } else {
