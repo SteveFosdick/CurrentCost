febc00cfaad9cb33078a4ce4deeed278366ddde4
Steve Fosdick <steve@fosdick.me.uk>
2020-03-16 01:12:40 +0000

    Revert "Return to file only logging."
    This reverts commit ea75f43b28494c01b53957d69efec23740d5c92f.

--- src/logger.h
+++ src/logger.h
@@ -1,11 +1,11 @@
 #ifndef LOGGER_INC
 #define LOGGER_INC
 
-#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _logger_t logger_t;
 
-extern logger_t *logger_new();
+extern logger_t *logger_new(const char *db_conn);
 extern void logger_free(logger_t * logger);
 
 extern void logger_data(logger_t * logger,
